39463e292e0e55f231825269a2641168e29d735c
Unknown <lucafloreau@akeonet.com>
2018-12-11 00:54:05 +0100

    Anim start end

--- Assets/Script/EndMenu.cs
+++ Assets/Script/EndMenu.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI winText;
-	// Use this for initialization
-	void Start () {
+
+    [SerializeField] private PlayableDirector timeLineLoose;
+    [SerializeField] private PlayableDirector timeLineWin;
+
+    // Use this for initialization
+    void Start () {
 	    if (GlobalGameManager.Instance.Win)
 	    {
 	        winText.text = "Win";
+            timeLineWin.Play();
 	    }
 	    else
 	    {
 	        winText.text = "GameOver";
+            timeLineLoose.Play();
         }
 
     }
